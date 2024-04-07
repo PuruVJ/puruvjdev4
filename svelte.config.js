33c69edda73533747312af25493ab5b4ba49d236
Puru <awesomepuruvj@gmail.com>
2024-04-07 14:14:28 +0530

    Push

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 // @ts-check
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({ precompress: true }),
   },
 
   preprocess: preprocess(),
