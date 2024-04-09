788afe3bf08c12232f12f62a6ab77de32b067260
Puru <awesomepuruvj@gmail.com>
2024-04-09 22:06:15 +0530

    Fix

--- src/routes/theme/+server.ts
+++ src/routes/theme/+server.ts
@@ -4,7 +4,7 @@ export async function POST({ cookies, request }) {
   const formdata = await request.formData();
   const theme = formdata.get('theme');
 
-  cookies.set('theme', theme + '' ?? 'light', { expires: new Date('2038-01-19'), path: '/' });
+  cookies.set('theme', theme.toString() ?? 'light', { expires: new Date('2038-01-19'), path: '/' });
 
   return json({ theme });
 }
