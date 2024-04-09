1d3f2c097a2d0e59b27c6d1d231b2c89b1191b07
Puru <awesomepuruvj@gmail.com>
2024-04-09 23:24:04 +0530

    avoid setheaders

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,8 @@
 export const load = ({ cookies, setHeaders }) => {
   // Strongly cache for CDN(1y), and for browser(1d). Stale while revalidate for both
-  setHeaders({
-    'Cache-Control': 'public, max-age=31536000, s-maxage=86400, stale-while-revalidate=86400',
-  });
+  // setHeaders({
+  //   'Cache-Control': 'public, max-age=31536000, s-maxage=86400, stale-while-revalidate=86400',
+  // });
 
   return {
     theme: cookies.get('theme'),
