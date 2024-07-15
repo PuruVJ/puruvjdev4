ef4dba71dcbd53d55f5447aa5969dd130e0ba5ec
Puru Vijay <awesomepuruvj@gmail.com>
2024-07-15 18:17:50 +0530

    Dummy migrate

--- src/routes/blog/migrate.ts
+++ src/routes/blog/migrate.ts
@@ -3,19 +3,13 @@ import { Likes } from '$lib/server/likes';
 async function migrate() {
   const db = Likes.db;
 
-  db.exec('CREATE TABLE IF NOT EXISTS likes (blogID TEXT PRIMARY KEY, likes INTEGER)');
+  //  Read all likes
+  const likes = db.query('SELECT * FROM likes').all() as { blogID: string; likes: number }[];
 
-  // Reads the likes.json file and inserts into the database
-  const file_path =
-    process.env.NODE_ENV === 'development'
-      ? new URL('./likes.json', import.meta.url).pathname
-      : '/likes.json';
-  const likes_obj = (await Bun.file(file_path).json()) as Record<string, number>;
-
-  console.log(likes_obj);
-
-  for (const [blogID, likes] of Object.entries(likes_obj)) {
-    Likes.set(blogID, likes);
+  // Now replace all likes with a random number between 100 and 2000
+  for (const { blogID } of likes) {
+    const newLikes = Math.floor(Math.random() * (2000 - 100) + 100);
+    Likes.set(blogID, newLikes);
   }
 }
 
